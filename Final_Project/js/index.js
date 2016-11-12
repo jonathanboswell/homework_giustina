652f0ccd2dd60615718f215367671099329125de
gclarencesmith <giustina@Giustinas-MacBook-Pro.local>
2016-11-11 16:48:54 -0800

    js + final css

--- Final_Project/js/index.js
+++ Final_Project/js/index.js
@@ -1,1 +1,26 @@
 console.log("hello!")
+
+$("section").hide();
+$("#home").show();
+
+
+$(function() {
+	$("#homeLink").click(function(){
+		$("section").hide();
+		$("#home").show();
+	});
+
+    $('li').click(function(){
+    	console.log($(this).attr('name'));
+    	//get the name from the <li> tag
+    	var name = $(this).attr('name');
+
+    	//make the clicked <li> "active"
+        $('li').removeClass();
+        $(this).addClass('active');
+
+        //show the corresponding <section>
+        $('section').hide();
+        $("#"+name).show();
+    })    
+});
